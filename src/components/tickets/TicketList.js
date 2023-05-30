fafdc6c67bc0f3e58028dd716df1680ce1dd05ac
Ryan Phillips <ryanmphill@gmail.com>
2023-05-30 11:31:57 -0500

    Fetched tickets to display and added styling

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,34 @@
-export const TicketList = () => {
-    return <h2>List of Tickets</h2>
-}
+import { useEffect, useState } from "react"
+import "./Tickets.css"
 
+export const TicketList = () => {
+    const [tickets, setTickets] = useState([])
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/serviceTickets`)
+                .then(response => response.json())
+                .then((ticketArray) => {
+                    setTickets(ticketArray)
+                })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+    return <>
+        <h2>List of Tickets</h2>
+
+        <article className="tickets">
+            {
+                tickets.map(
+                    (ticket) => {
+                        return <section className="ticket">
+                            <header>{ticket.description}</header>
+                            <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
+                        </section>
+                    }
+                )
+            }
+
+        </article>
+    </>
+}
